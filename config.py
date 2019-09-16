d6273a057df2e58b83c6261ca7967d4d2778338e
Santa Claus <joselynetusingwir@gmail.com>
2019-09-16 17:49:45 +0200

    first commit

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
     ARTICLES_URL ='https://newsapi.org/v2/everything?q={}&apiKey={}'
-    # ARTICLES_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+   
     HEADLINES_URL ='https://newsapi.org/v2/top-headlines?language={}&apiKey={}'
     NEWS_API_KEY ='f85b298aaced4c6793886941d5c0f899'
 
